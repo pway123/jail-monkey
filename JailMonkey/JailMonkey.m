a576adfa5656868949f39710bbf359de5dc09b00
pway123 <46521199+pway123@users.noreply.github.com>
2019-03-14 14:48:28 +0800

    Add debugger check for iOS

--- JailMonkey/JailMonkey.m
+++ JailMonkey/JailMonkey.m
@@ -9,9 +9,24 @@
 #import "JailMonkey.h"
 @import UIKit;
 
+// For debugger detection
+#import <dlfcn.h>
+#import <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/sysctl.h>
+#include <unistd.h>
+
 static NSString * const JMJailbreakTextFile = @"/private/jailbreak.txt";
 static NSString * const JMisJailBronkenKey = @"isJailBroken";
 static NSString * const JMCanMockLocationKey = @"canMockLocation";
+static NSString * const JMisDebuggerAttachedKey = @"isDebuggerAttached";
+
+typedef int (*ptrace_ptr_t)(int _request, pid_t _pid, caddr_t _addr, int _data);
+
+#if !defined(PT_DENY_ATTACH)
+#define PT_DENY_ATTACH 31
+#endif  // !defined(PT_DENY_ATTACH)
 
 @implementation JailMonkey
 
@@ -89,11 +104,62 @@ RCT_EXPORT_MODULE();
     return [self checkPaths] || [self checkSchemes] || [self canViolateSandbox];
 }
 
+/*
+ * Ref: https://github.com/vtky/ios-antidebugging/blob/master/antidebugging/main.m
+*/
+- (BOOL)checkPtrace{
+  void* handle = dlopen(0, RTLD_GLOBAL | RTLD_NOW);
+  ptrace_ptr_t ptrace_ptr = dlsym(handle, "ptrace");
+  ptrace_ptr(PT_DENY_ATTACH, 0, 0, 0);
+  dlclose(handle);
+  
+  return NO;
+}
+
+/*
+ * Ref: https://github.com/vtky/ios-antidebugging/blob/master/antidebugging/main.m
+*/
+- (BOOL)checkSysctl{
+  int mib[4];
+  struct kinfo_proc info;
+  size_t info_size = sizeof(info);
+
+  // Initialize the flags so that, if sysctl fails for some bizarre
+  // reason, we get a predictable result.
+
+  info.kp_proc.p_flag = 0;
+
+  // Initialize mib, which tells sysctl the info we want, in this case
+  // we're looking for information about a specific process ID.
+
+  mib[0] = CTL_KERN;
+  mib[1] = KERN_PROC;
+  mib[2] = KERN_PROC_PID;
+  mib[3] = getpid();
+
+  // Call sysctl.
+
+  if (sysctl(mib, 4, &info, &info_size, NULL, 0) == -1)
+  {
+      perror("perror sysctl");
+      exit(-1);
+  }
+
+  // We're being debugged if the P_TRACED flag is set.
+  
+  return ((info.kp_proc.p_flag & P_TRACED) != 0);
+}
+
+- (BOOL)isDebuggerAttached{
+    return [self checkPtrace] || [self checkSysctl];
+}
+
 - (NSDictionary *)constantsToExport
 {
 	return @{
 			 JMisJailBronkenKey: @(self.isJailBroken),
-			 JMCanMockLocationKey: @(self.isJailBroken)
+			 JMCanMockLocationKey: @(self.isJailBroken),
+			 JMisDebuggerAttachedKey: @(self.isDebuggerAttached)
 			 };
 }
 
