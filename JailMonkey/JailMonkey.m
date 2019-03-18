3b13254ca74280e186ba48f3cdf686ff36766bdc
pway123 <46521199+pway123@users.noreply.github.com>
2019-03-18 15:25:57 +0800

    Removed debugger checks

--- JailMonkey/JailMonkey.m
+++ JailMonkey/JailMonkey.m
@@ -37,13 +37,6 @@ RCT_EXPORT_MODULE();
     return YES;
 }
 
-- (NSArray *)debuggerPathToCheck
-{
-    return @[
-             @"/var/lib/cydia"
-             ];
-}
-
 - (NSArray *)pathsToCheck
 {
     return @[
@@ -162,63 +155,11 @@ RCT_EXPORT_MODULE();
     return [self checkPaths] || [self checkSchemes] || [self canViolateSandbox] || [self isBlacklistedPaths:pathToCheck];
 }
 
-/*
- * Ref: https://github.com/vtky/ios-antidebugging/blob/master/antidebugging/main.m
-*/
-- (BOOL)checkPtrace{
-  void* handle = dlopen(0, RTLD_GLOBAL | RTLD_NOW);
-  ptrace_ptr_t ptrace_ptr = dlsym(handle, "ptrace");
-  ptrace_ptr(PT_DENY_ATTACH, 0, 0, 0);
-  dlclose(handle);
-  
-  return NO;
-}
-
-/*
- * Ref: https://github.com/vtky/ios-antidebugging/blob/master/antidebugging/main.m
-*/
-- (BOOL)checkSysctl{
-  int mib[4];
-  struct kinfo_proc info;
-  size_t info_size = sizeof(info);
-
-  // Initialize the flags so that, if sysctl fails for some bizarre
-  // reason, we get a predictable result.
-
-  info.kp_proc.p_flag = 0;
-
-  // Initialize mib, which tells sysctl the info we want, in this case
-  // we're looking for information about a specific process ID.
-
-  mib[0] = CTL_KERN;
-  mib[1] = KERN_PROC;
-  mib[2] = KERN_PROC_PID;
-  mib[3] = getpid();
-
-  // Call sysctl.
-
-  if (sysctl(mib, 4, &info, &info_size, NULL, 0) == -1)
-  {
-      perror("perror sysctl");
-      exit(-1);
-  }
-
-  // We're being debugged if the P_TRACED flag is set.
-  
-  return ((info.kp_proc.p_flag & P_TRACED) != 0);
-}
-
-- (BOOL)isDebuggerAttached{
-    NSArray* pathToCheck = [self debuggerPathToCheck];
-    return [self checkPtrace] || [self checkSysctl] || [self isBlacklistedPaths:pathToCheck];
-}
-
 - (NSDictionary *)constantsToExport
 {
 	return @{
 			 JMisJailBronkenKey: @(self.isJailBroken),
-			 JMCanMockLocationKey: @(self.isJailBroken),
-			 JMisDebuggerAttachedKey: @(self.isDebuggerAttached)
+			 JMCanMockLocationKey: @(self.isJailBroken)
 			 };
 }
 
