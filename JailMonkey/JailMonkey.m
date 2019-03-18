f99cab6cd0c4402167633c80cb708c4e04b85a54
pway123 <46521199+pway123@users.noreply.github.com>
2019-03-18 15:40:43 +0800

    Added debugger checks

--- JailMonkey/JailMonkey.m
+++ JailMonkey/JailMonkey.m
@@ -9,9 +9,24 @@
 #import "JailMonkey.h"
 @import UIKit;
 
+// For debugger detection
+#import <dlfcn.h>
+#import <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/sysctl.h>
+#include <unistd.h>
+
 static NSString * const JMJailbreakTextFile = @"/private/jailbreak.txt";
 static NSString * const JMisJailBronkenKey = @"isJailBroken";
 static NSString * const JMCanMockLocationKey = @"canMockLocation";
+static NSString * const JMisDebuggerAttachedKey = @"isDebuggerAttached";
+
+typedef int (*ptrace_ptr_t)(int _request, pid_t _pid, caddr_t _addr, int _data);
+
+#if !defined(PT_DENY_ATTACH)
+#define PT_DENY_ATTACH 31
+#endif  // !defined(PT_DENY_ATTACH)
 
 @implementation JailMonkey
 
@@ -20,6 +35,13 @@ RCT_EXPORT_MODULE();
 + (BOOL)requiresMainQueueSetup
 {
     return YES;
+}
+
+- (NSArray *)debuggerPathToCheck
+{
+    return @[
+             @"/var/lib/cydia"
+             ];
 }
 
 - (NSArray *)pathsToCheck
@@ -87,7 +109,7 @@ RCT_EXPORT_MODULE();
             existsPath = YES;
         }
     }
-    
+ 
     return existsPath;
 }
 
@@ -120,15 +142,15 @@ RCT_EXPORT_MODULE();
 }
 
 - (BOOL)canViolateSandbox{
-    NSError *error;
+	NSError *error;
     BOOL grantsToWrite = NO;
-    NSString *stringToBeWritten = @"This is an anti-spoofing test.";
-    [stringToBeWritten writeToFile:JMJailbreakTextFile atomically:YES
-                          encoding:NSUTF8StringEncoding error:&error];
-    if(!error){
-        //Device is jailbroken
-        grantsToWrite = YES;
-    }
+	NSString *stringToBeWritten = @"This is an anti-spoofing test.";
+	[stringToBeWritten writeToFile:JMJailbreakTextFile atomically:YES
+						  encoding:NSUTF8StringEncoding error:&error];
+	if(!error){
+		//Device is jailbroken
+		grantsToWrite = YES;
+	}
     
     [[NSFileManager defaultManager] removeItemAtPath:JMJailbreakTextFile error:nil];
     
@@ -140,13 +162,64 @@ RCT_EXPORT_MODULE();
     return [self checkPaths] || [self checkSchemes] || [self canViolateSandbox] || [self isBlacklistedPaths:pathToCheck];
 }
 
+/*
+ * Ref: https://github.com/vtky/ios-antidebugging/blob/master/antidebugging/main.m
+*/
+- (BOOL)checkPtrace{
+  void* handle = dlopen(0, RTLD_GLOBAL | RTLD_NOW);
+  ptrace_ptr_t ptrace_ptr = dlsym(handle, "ptrace");
+  ptrace_ptr(PT_DENY_ATTACH, 0, 0, 0);
+  dlclose(handle);
+  
+  return NO;
+}
+
+/*
+ * Ref: https://github.com/vtky/ios-antidebugging/blob/master/antidebugging/main.m
+*/
+- (BOOL)checkSysctl{
+  int mib[4];
+  struct kinfo_proc info;
+  size_t info_size = sizeof(info);
+
+  // Initialize the flags so that, if sysctl fails for some bizarre
+  // reason, we get a predictable result.
+
+  info.kp_proc.p_flag = 0;
+
+  // Initialize mib, which tells sysctl the info we want, in this case
+  // we're looking for information about a specific process ID.
+
+  mib[0] = CTL_KERN;
+  mib[1] = KERN_PROC;
+  mib[2] = KERN_PROC_PID;
+  mib[3] = getpid();
+
+  // Call sysctl.
+
+  if (sysctl(mib, 4, &info, &info_size, NULL, 0) == -1)
+  {
+      perror("perror sysctl");
+      exit(-1);
+  }
+
+  // We're being debugged if the P_TRACED flag is set.
+  
+  return ((info.kp_proc.p_flag & P_TRACED) != 0);
+}
+
+- (BOOL)isDebuggerAttached{
+    NSArray* pathToCheck = [self debuggerPathToCheck];
+    return [self checkPtrace] || [self checkSysctl] || [self isBlacklistedPaths:pathToCheck];
+}
+
 - (NSDictionary *)constantsToExport
 {
-    return @{
-             JMisJailBronkenKey: @(self.isJailBroken),
-             JMCanMockLocationKey: @(self.isJailBroken)
-             };
+	return @{
+			 JMisJailBronkenKey: @(self.isJailBroken),
+			 JMCanMockLocationKey: @(self.isJailBroken),
+			 JMisDebuggerAttachedKey: @(self.isDebuggerAttached)
+			 };
 }
 
 @end
-
