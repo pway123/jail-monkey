5741cc497ecbe4420c37de6a79008000e2b28e06
Shek Yu Hong <shek_yu_hong@tech.gov.sg>
2019-06-03 02:06:47 +0800

    Exposed method to return breakdown of various rooted checks result as object in js

--- android/src/main/java/com/gantix/JailMonkey/JailMonkeyModule.java
+++ android/src/main/java/com/gantix/JailMonkey/JailMonkeyModule.java
@@ -11,6 +11,7 @@ import java.io.*;
 import static com.gantix.JailMonkey.ExternalStorage.ExternalStorageCheck.isOnExternalStorage;
 import static com.gantix.JailMonkey.MockLocation.MockLocationCheck.isMockLocationOn;
 import static com.gantix.JailMonkey.Rooted.RootedCheck.isJailBroken;
+import static com.gantix.JailMonkey.Rooted.RootedCheck.getRootedCheckInfoBreakdown;
 
 public class JailMonkeyModule extends ReactContextBaseJavaModule {
 
@@ -40,6 +41,11 @@ public class JailMonkeyModule extends ReactContextBaseJavaModule {
     final Map<String, Object> constants = new HashMap<>();
     boolean jailBreak = isJailBroken(context);
     boolean isMagiskHidden = isMagiskHidden();
+    
+    HashMap rootedCheckInfoBreakdown = getRootedCheckInfoBreakdown(context);
+    rootedCheckInfoBreakdown.put("isMagiskHidden", isMagiskHidden());
+    rootedCheckInfoBreakdown.put("rootBeerRootDetectionBreakdown", getRootBeerRootDetectionBreakdown(context));
+    constants.put("getRootedCheckInfoBreakdown", rootedCheckInfoBreakdown);
 
     constants.put("isJailBroken", jailBreak || isMagiskHidden);
     constants.put("canMockLocation", isMockLocationOn(context));
--- android/src/main/java/com/gantix/JailMonkey/Rooted/RootedCheck.java
+++ android/src/main/java/com/gantix/JailMonkey/Rooted/RootedCheck.java
@@ -1,8 +1,10 @@
 package com.gantix.JailMonkey.Rooted;
 
 import android.content.Context;
+import java.util.HashMap;
 
 import com.scottyab.rootbeer.RootBeer;
+
 import android.os.Build;
 
 public class RootedCheck {
@@ -11,6 +13,7 @@ public class RootedCheck {
     private static final String MOTO = "moto";
     private static final String XIAOMI = "xiaomi";
     private static final String OPPO = "oppo";
+    private static HashMap rootedCheckBreakdown = new HashMap();
 
     /**
      * Checks if the device is rooted.
@@ -40,5 +43,43 @@ public class RootedCheck {
             rv = rootBeer.isRooted();
         }
         return rv;
+    }
+
+    public static HashMap getRootedCheckInfoBreakdown(Context context){
+        final String brand = Build.BRAND.toLowerCase();
+        RootBeer rootBeer = new RootBeer(context);
+        boolean isBrandsToSkipBusybox = (brand.contains(ONEPLUS) || brand.contains(MOTO) || brand.contains(XIAOMI) || brand.contains(OPPO));
+
+        CheckApiVersion check;
+        if (android.os.Build.VERSION.SDK_INT >= 23) {
+            check = new GreaterThan23();
+        } else {
+            check = new LessThan23();
+        }
+
+        rootedCheckBreakdown.put("sdkVersion", android.os.Build.VERSION.SDK_INT);
+        rootedCheckBreakdown.put("jailMonkeyIsSuperUserPresent", check.checkRooted());
+        rootedCheckBreakdown.put("rootBeerIsRooted", rootBeerCheck(context));
+        rootedCheckBreakdown.put("shouldSkipBusyBoxCheck", isBrandsToSkipBusybox);
+
+        return rootedCheckBreakdown;
+    }
+
+    public static HashMap getRootBeerRootDetectionBreakdown(Context context){
+        HashMap rootedBeerCheckBreakdown = new HashMap();
+        RootBeer rootBeer = new RootBeer(context);
+        
+        rootedBeerCheckBreakdown.put("detectRootManagementApps", rootBeer.detectRootManagementApps());
+        rootedBeerCheckBreakdown.put("detectPotentiallyDangerousApps", rootBeer.detectPotentiallyDangerousApps());
+        rootedBeerCheckBreakdown.put("checkForBinarySu", rootBeer.checkForBinary("su"));
+        rootedBeerCheckBreakdown.put("checkForBinaryBusybox", rootBeer.checkForBinary("busybox"));
+        rootedBeerCheckBreakdown.put("checkForDangerousProps", rootBeer.checkForDangerousProps());
+        rootedBeerCheckBreakdown.put("checkForRWPaths", rootBeer.checkForRWPaths());
+        rootedBeerCheckBreakdown.put("detectTestKeys", rootBeer.detectTestKeys());
+        rootedBeerCheckBreakdown.put("checkSuExists", rootBeer.checkSuExists());
+        rootedBeerCheckBreakdown.put("checkForRootNative", rootBeer.checkForRootNative());
+        rootedBeerCheckBreakdown.put("checkForMagiskBinary", rootBeer.checkForMagiskBinary());
+
+        return rootedBeerCheckBreakdown;
     }
 }
